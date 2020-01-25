03ee2a18b784a058048aa1697726c5ca342084f3
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-25 20:17:33 +0300

    Scripted auto-commit on change (2020-01-25 20:17:33) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -1,9 +1,11 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class StoreActor extends AbstractActor {
     @Override
     public Receive createReceive(){
-
+        return ReceiveBuilder.create()
+                
     }
 
 }
