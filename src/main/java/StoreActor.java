d41abf3ee7dc93edf4337db8cfccd9e59c7a7595
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-25 20:39:40 +0300

    Scripted auto-commit on change (2020-01-25 20:39:40) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -1,12 +1,19 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class StoreActor extends AbstractActor {
+    private ArrayList<String> addresses;
+    private Random randomServer;
+
+
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
                 .match(GetMessage.class, m ->
-                        getSender().tell(new RandomServerMessage))
+                        getSender().tell(new RandomServerMessage(addresses.get(randomServer.nextInt(addresses.size()))), self()))
 
     }
 
