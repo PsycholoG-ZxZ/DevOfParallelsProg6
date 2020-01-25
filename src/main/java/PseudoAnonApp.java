1c04aefb21144f48f83a57c534e9430c8e799bd1
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-25 20:01:37 +0300

    Scripted auto-commit on change (2020-01-25 20:01:37) by gitwatch.sh

--- src/main/java/PseudoAnonApp.java
+++ src/main/java/PseudoAnonApp.java
@@ -1,6 +1,7 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.KeeperException;
 
 import java.io.IOException;
@@ -9,7 +10,7 @@ public class PseudoAnonApp {
     public static void main (String[] args) throws InterruptedException, KeeperException, IOException {
         ActorSystem system = ActorSystem.create("anonymizer");
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class));
-
+        final ActorMaterializer materializer = ActorMaterializer.create()
 
 
     }
