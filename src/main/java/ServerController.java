2293f595669370cea91ae894fafc07b8ff15b033
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 13:40:55 +0300

    Scripted auto-commit on change (2020-01-26 13:40:55) by gitwatch.sh

--- src/main/java/ServerController.java
+++ src/main/java/ServerController.java
@@ -16,7 +16,7 @@ public class ServerController {
         watchChildrenCallback(null);
     }
 
-    private void watchChildrenCallback(WatchEvent event){
+    private void watchChildrenCallback(WatchedEvent event){
         try{
             this.storeActor.tell(new AllServersMessage(zoo.getChildren("/servers", this::watchChildrenCallback).stream()
             .map(s -> "/servers " + s).collect(Collectors.toCollection(ArrayList::new))), ActorRef.noSender());
