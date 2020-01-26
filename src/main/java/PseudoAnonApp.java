efa7e5c9033f7341c927ceb9c53c1e5e32a350c7
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 13:53:20 +0300

    Scripted auto-commit on change (2020-01-26 13:53:20) by gitwatch.sh

--- src/main/java/PseudoAnonApp.java
+++ src/main/java/PseudoAnonApp.java
@@ -26,7 +26,9 @@ public class PseudoAnonApp {
         Logger log = Logger.getLogger(PseudoAnonApp.class.getName());
         ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 5000, loger -> log.info(loger.toString()));
 
-        ServerController server = new ServerController(storeActor, zoo);
+        String link = "localhost" + port;
+
+        ServerController server = new ServerController(storeActor, zoo, link);
 
 
 
