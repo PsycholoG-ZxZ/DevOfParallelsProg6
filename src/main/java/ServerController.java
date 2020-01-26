c3256f1a252f4870bd27b0502fb23d9d30093f7e
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 14:01:49 +0300

    Scripted auto-commit on change (2020-01-26 14:01:49) by gitwatch.sh

--- src/main/java/ServerController.java
+++ src/main/java/ServerController.java
@@ -1,5 +1,6 @@
 import akka.actor.Actor;
 import akka.actor.ActorRef;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
@@ -17,8 +18,8 @@ public class ServerController {
         this.zoo = zoo;
         watchChildrenCallback(null);
         String port = link.substring(link.length()-5, link.length()-1);
-        zoo.create("/servers" + "/" + link, host + ":" + port,
-                ZooDefs.Ids.OPEN_ACL_UNSAFE)
+        zoo.create("/servers" + "/" + link, (host + ":" + port).getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 
     }
 
