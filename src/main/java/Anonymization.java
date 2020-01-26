c2823cf6275015609ef10997dd4437a9d48d5018
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 17:23:23 +0300

    Scripted auto-commit on change (2020-01-26 17:23:23) by gitwatch.sh

--- src/main/java/Anonymization.java
+++ src/main/java/Anonymization.java
@@ -1,10 +1,12 @@
 import org.apache.zookeeper.ZooKeeper;
+import org.asynchttpclient.AsyncHttpClient;
 
 public class Anonymization {
     private ZooKeeper zoo;
+    private AsyncHttpClient asyncHttp;
 
-    public Anonymization(ZooKeeper zoo){
+    public Anonymization(ZooKeeper zoo, AsyncHttpClient http){
         this.zoo = zoo;
-
+        this.asyncHttp = http;
     }
 }
