efc6259cb29edb4b03a3a0540d500cec24ab2ae2
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 18:00:16 +0300

    Scripted auto-commit on change (2020-01-26 18:00:16) by gitwatch.sh

--- src/main/java/Anonymization.java
+++ src/main/java/Anonymization.java
@@ -26,7 +26,7 @@ public class Anonymization {
                     if (count_int == 0){
                         response = fetch(asyncHttp.prepareGet(url).build());
                     }else{
-
+                        response = 
                     }
                     return completeOKWithFutureString(response.thenApply(Response::getResponseBody));
 
