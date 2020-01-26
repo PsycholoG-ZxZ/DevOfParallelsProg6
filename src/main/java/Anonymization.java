a5ed8b348ba12205918fc3aa528a1f72033883c1
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-26 17:45:32 +0300

    Scripted auto-commit on change (2020-01-26 17:45:32) by gitwatch.sh

--- src/main/java/Anonymization.java
+++ src/main/java/Anonymization.java
@@ -23,10 +23,8 @@ public class Anonymization {
                 {
                     CompletionStage<Response> resp;
 
-
-                    }
                 }))
+        );
 
-        )
     }
 }
