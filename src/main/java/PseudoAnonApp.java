f097859c9426657e8ea81574d4a1a94675354c68
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-28 17:44:41 +0300

    Scripted auto-commit on change (2020-01-28 17:44:41) by gitwatch.sh

--- src/main/java/PseudoAnonApp.java
+++ src/main/java/PseudoAnonApp.java
@@ -45,7 +45,7 @@ public class PseudoAnonApp {
         asyncHttpClient.close();
         server.removerWatches();
         zoo.close();
-        bind.thenCompose(ServerBinding::unbind).thenAccept(unbo)
+        bind.thenCompose(ServerBinding::unbind).thenAccept(unbound.)
 
 
 
