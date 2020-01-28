efd5eb30488c13ca401f1921162bc4d8f9b5f39b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-28 20:25:09 +0300

    Scripted auto-commit on change (2020-01-28 20:25:09) by gitwatch.sh

--- src/main/java/ServerController.java
+++ src/main/java/ServerController.java
@@ -15,8 +15,6 @@ public class ServerController {
     private ActorRef storeActor;
     private ZooKeeper zoo;
 
-
-
     public ServerController(ActorRef store, ZooKeeper zoo, String link, String host) throws InterruptedException, KeeperException{
         this.storeActor = store;
         this.zoo = zoo;
@@ -26,11 +24,13 @@ public class ServerController {
         zoo.create(SERVERS_PATH_WITH_SLASH + link, (host + ":" + port).getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);   //Создаем узел (слайд 22)
     }
 
-
-
     private void watchChildrenCallback(WatchedEvent event){
         try{
-            /* Получение данных об узлах: getChildren - получение дочерних узлов */
+            /* Получение данных об узлах: getChildren - получение дочерних узлов
+             * Каждый сервер регистрируется в zookeeper - создает дочерний узел для узла
+             * /servers в содержимом которого хранит свой порт а также подписывается на
+             * изменения servers.
+             */
             this.storeActor.tell(new AllServersMessage(zoo.getChildren(SERVERS_PATH, this::watchChildrenCallback).stream()
             .map(s -> SERVERS_PATH_WITH_SLASH + s).collect(Collectors.toCollection(ArrayList::new))), ActorRef.noSender());
         }catch (Exception ex){
@@ -41,6 +41,5 @@ public class ServerController {
 
     public void removerWatches() throws KeeperException, InterruptedException{
         zoo.removeAllWatches(SERVERS_PATH, Watcher.WatcherType.Any, true);
-
     }
 }
