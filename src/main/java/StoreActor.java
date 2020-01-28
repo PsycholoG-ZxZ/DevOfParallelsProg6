311c6663f56c25ad970f1d75c3fb6a0ea6c59237
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2020-01-28 18:15:02 +0300

    Scripted auto-commit on change (2020-01-28 18:15:02) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -16,12 +16,12 @@ public class StoreActor extends AbstractActor {
                         getSender().tell(new RandomServerMessage(addresses.get(randomServer.nextInt(addresses.size()))), self()))
 
                 .match(AllServersMessage.class, m ->{ ArrayList<String> newServers = m.getAllServers();
-                        if (addresses.size() == 0) {
-                            this.addresses.addAll(newServers);
-                        }else{
+         //               if (addresses.size() == 0) {
+         //                   this.addresses.addAll(newServers);
+         //               }else{
                             this.addresses.clear();
                             this.addresses.addAll(newServers);
-                        }
+         //               }
                 })
 
                 .match(RandomServerMessage.class, m -> this.addresses.remove(m.getServer()))
